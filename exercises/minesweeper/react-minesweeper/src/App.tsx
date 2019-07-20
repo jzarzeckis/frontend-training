aa5925d451a05c304e4a00bf90e679d37beb8224
Janis Zarzeckis <jzarzeckis@evolutiongaming.com>
2019-07-20 13:32:21 +0300

    Add some comments

--- exercises/minesweeper/react-minesweeper/src/App.tsx
+++ exercises/minesweeper/react-minesweeper/src/App.tsx
@@ -9,8 +9,9 @@ enum Space {
 }
 
 type position = [number, number];
+// Array containing **relative** indices of the cells around some cell
 const relativeNeighbors: position[] = [
-  [   -1, -1], [0, -1], [1, -1],
+  [-1, -1], [0, -1], [1, -1],
   [-1, 0], [1, 0],
   [-1, 1], [0, 1], [1, 1]
 ]
@@ -36,16 +37,22 @@ function nearbyBombCount(
   cellIndex: number,
   matrix: Array<Array<Space>>
 ) {
+  // Iterate through all neighbors of this cell, and count bombs
   return relativeNeighbors.reduce((count, relativePos) => {
+    // Turn the relative position absolute by adding it to current cell index
     const rowIdToCheck = rowIndex + relativePos[1];
     const cellIdToCheck = cellIndex + relativePos[0];
     if (rowIdToCheck < 0 || rowIdToCheck >= matrix.length) {
+      // If it is outside bounds, don't compare, and just return count
       return count;
     }
+    // Now space is guaranteed to be within bounds
     const space = matrix[rowIdToCheck][cellIdToCheck];
     if (space === Space.HiddenBomb || space === Space.ExplodedBomb) {
+      // Hidden bombs and exploded ones are counted as 1;
       return count + 1;
     }
+    // If this code is reached, it's like an else - where the currently accumulated count is retained
     return count;
   }, 0);
 }
@@ -58,6 +65,10 @@ function isCellExploded(cell: Space) {
   }
 }
 
+/**
+ * Returns boolean value whether any of the cells inside data has type ExplodedBomb
+ * @param data The matrix of all the cells in this minesweeper
+ */
 function isGameOver(data: Space[][]) {
   return data.some((row) =>
     row.some(cell => cell === Space.ExplodedBomb)
@@ -68,16 +79,20 @@ function stateAfterClick(prevState: Space[][], cellClicked: number, rowClicked: 
   // Change state of the clicked row to switch color
   const nextState = prevState.map((row, rowIndex) => 
     row.map((cell, cellIndex) => {
+      // This function is called for **EVERY** cell in the matrix
       if (rowIndex === rowClicked && cellIndex === cellClicked) {
+        // If we're in this block, it means we're looking at the cell that was clicked
         if (cell === Space.HiddenBomb || cell === Space.ExplodedBomb) {
           return Space.ExplodedBomb;
         } else {
           return Space.ClickedSpace;
         }
       } else {
+        // Cells that were not clicked are not changed, so we return the same element in the map call
         return cell;
       }
     }));
+
   // Check if cell clicked has 0 neighbor bombs
   if (
     nextState[rowClicked][cellClicked] === Space.ClickedSpace &&
@@ -114,6 +129,8 @@ const App: React.FC = () => {
   const [ data, setData ] = useState(generateRandomFields(width, height));
 
   function fieldClicked(rowClicked: number = 0, cellClicked: number = 0) {
+    // When field is clicked, we check how would that affect the state,
+    // and update it in react, so that react would render it
     setData(stateAfterClick(data, cellClicked, rowClicked));
   }
 
